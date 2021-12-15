bf01b98a21fa45fcbece3333166ff6b8bd0c306d
Nicolas DUBOUILH <ndubouil@e2r4p1.clusters.42paris.fr>
2021-12-15 15:24:11 +0100

    accuracy ok

--- 5-Neural_Networks/traffic/traffic.py
+++ 5-Neural_Networks/traffic/traffic.py
@@ -83,23 +83,36 @@ def get_model():
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
 
+    # https://towardsdatascience.com/traffic-sign-recognition-with-tensorflow-2-x-7ab4e8611a83
+    # https://www.tensorflow.org/tutorials/keras/classification
+
     model = tf.keras.models.Sequential([
         # Convolutional layer. Learn 32 filters using a 3x3 kernel
         tf.keras.layers.Conv2D(
             32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
-
+        # tf.keras.layers.Conv2D(
+        #     6, (5, 5), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        # ),
         # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu"
+        ),
+        # tf.keras.layers.Conv2D(
+            # 16, (5, 5), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        # ),
+
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
         # Flatten units
         tf.keras.layers.Flatten(),
 
         # Add a hidden layer with dropout
-        # tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(120, activation="relu"),
+        tf.keras.layers.Dense(128, activation="relu"),
+        # tf.keras.layers.Dense(120, activation="relu"),
         # tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(84, activation="relu"),
+        # tf.keras.layers.Dense(84, activation="relu"),
         tf.keras.layers.Dropout(0.5),
 
         # Add an output layer with output units for all 10 digits
